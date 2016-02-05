719f865cbce25d9f9fff67efb1d73c9cd52e73b8
Andrew McCarthy <hi@andrevv.com>
2016-02-05 21:57:20 +0100

    jahu

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ function scroll() {
   var docheight = $(document).height();
   if(scrollTop > docheight - section_h * 3) {
     count++;
-    if(count >= 10) {
+    if(count >= 20) {
       if($('#begin').find('#end').length == 0) {
         $("#end").appendTo('#begin').css("display","block");
       }
